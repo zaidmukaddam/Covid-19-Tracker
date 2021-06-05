d175de0acd81d9e501e195e4f07ec9d91bc595ba
zaidmukaddam <zaidaltafmukaddam@gmail.com>
2021-06-05 22:31:13 +0530

    Final Build 🔥

---
+++ lib/blocs/theme.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class ThemeChanger with ChangeNotifier {
+  ThemeData _themeData = ThemeData.light();
+
+  ThemeChanger(this._themeData);
+  bool initial = true;
+
+  getTheme() => initial ? _themeData = getLightTheme() : _themeData;
+
+  lightTheme() {
+    initial = false;
+    _themeData = getLightTheme();
+    notifyListeners();
+  }
+
+  darkTheme() {
+    initial = false;
+    _themeData = getDarkTheme();
+    notifyListeners();
+  }
+
+  getDarkTheme() {
+    _themeData = ThemeData.dark().copyWith(
+      scaffoldBackgroundColor: Colors.black,
+      appBarTheme: AppBarTheme(
+        color: Colors.black,
+        textTheme: TextTheme(
+          headline6: TextStyle(fontFamily: 'Poppins',fontSize: 18, color: Colors.white),
+        ),
+      ),
+    );
+    return _themeData;
+  }
+
+  getLightTheme() {
+    _themeData = ThemeData.light().copyWith(
+      scaffoldBackgroundColor: Colors.white,
+      appBarTheme: AppBarTheme(
+        color: Colors.white,
+        textTheme: TextTheme(
+          headline6: TextStyle(fontFamily: 'Poppins',fontSize: 18, color: Colors.black),
+        ),
+      ),
+    );
+    return _themeData;
+  }
+}
